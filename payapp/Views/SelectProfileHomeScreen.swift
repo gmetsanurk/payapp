6452e963951c65fc1d50d64d41deaf1113fd7abd
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-07-06 18:52:02 +0300

    More UI at Home screen

--- payapp/Views/SelectProfileHomeScreen.swift
+++ payapp/Views/SelectProfileHomeScreen.swift
@@ -10,13 +10,20 @@ import UIKit
 typealias CellDataType = ProfileModel
 class SelectProfileHomeScreen: UIViewController {
     
+    private let headerView = UIView()
+    private let titleLabel = UILabel()
+    private let coinsButton = UIButton(type: .custom)
+    private var segmentedControl = UISegmentedControl()
+    
     private weak var selectProfilesList:  SelectProfileHomeCollectionView<HomeScreenProfileCell, CellDataType>?
     
     private lazy var presenter = HomePresenter(view: self)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view.backgroundColor = .white
+        setupUI()
+        makeConstraints()
         
         let layout = UICollectionViewFlowLayout()
         layout.estimatedItemSize = .init(width: view.bounds.width - 32, height: 200)
@@ -28,7 +35,8 @@ class SelectProfileHomeScreen: UIViewController {
         view.addSubview(profilesList)
         self.selectProfilesList = profilesList
         profilesList.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
+            make.top.equalTo(headerView.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
         }
         DispatchQueue.main.async { [weak self] in
             self?.presenter.loadProfiles()
@@ -38,6 +46,64 @@ class SelectProfileHomeScreen: UIViewController {
     func displayProfiles(_ profiles: [CellDataType]) {
         selectProfilesList?.data = profiles
     }
+    
+}
 
+extension SelectProfileHomeScreen {
+    
+    func setupHeaderView() {
+        headerView.backgroundColor = .clear
+    }
+    
+    func setupTitleLabel() {
+        titleLabel.text = "Feed"
+        titleLabel.font = .systemFont(ofSize: 24, weight: .bold)
+    }
+    
+    func setupCoinsButton() {
+        let coinsImage = UIImage(named: "label-coins")?.withRenderingMode(.alwaysOriginal)
+        coinsButton.setImage(coinsImage, for: .normal)
+        coinsButton.imageView?.contentMode = .scaleAspectFit
+    }
+    
+    private func setupSegmentedControl() -> UISegmentedControl {
+        let items = ["Online", "Popular", "New", "Following"]
+        let sc = UISegmentedControl(items: items)
+        sc.selectedSegmentIndex = 0
+        return sc
+    }
+    
+    func setupUI() {
+        view.addSubview(headerView)
+        segmentedControl = setupSegmentedControl()
+        setupHeaderView()
+        headerView.addSubview(titleLabel)
+        headerView.addSubview(coinsButton)
+        headerView.addSubview(segmentedControl)
+        
+        setupTitleLabel()
+        setupCoinsButton()
+        
+    }
+    
+    func makeConstraints() {
+        
+        headerView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.leading.trailing.equalToSuperview()
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.top.leading.equalToSuperview().inset(16)
+        }
+        coinsButton.snp.makeConstraints { make in
+            make.centerY.equalTo(titleLabel)
+            make.trailing.equalToSuperview().inset(16)
+        }
+        segmentedControl.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(12)
+            make.leading.trailing.equalToSuperview().inset(16)
+            make.bottom.equalToSuperview().inset(8)
+        }
+    }
 }
 
