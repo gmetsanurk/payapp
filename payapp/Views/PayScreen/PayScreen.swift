89f0af63c9afdec4dd89bacb085c1e2e5a6ce886
Georgy Metsanurk <gmetsanurk@gmail.com>
2025-07-09 19:10:29 +0300

    Reorganize PayScreen and make layout

--- payapp/Views/PayScreen/PayScreen.swift
+++ payapp/Views/PayScreen/PayScreen.swift
@@ -32,15 +32,115 @@ final class PayScreen: UIViewController {
     private var subscribeButton = UIButton(type: .system)
     private let termsStack = UIStackView()
     private let termsButton = UIButton(type: .system)
+    private let privacyButton = UIButton(type: .system)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        createButton()
-        setupUI()
+        createSubscribeButton()
+        makeSlideControllers()
+        setupPageViewController()
+        setupPageControl()
+        setupBottomArea()
+        layoutAll()
+        
         viewModelReloadProfile()
     }
     
+}
+
+extension PayScreen {
+    
+    func createSubscribeButton() {
+        let button = UIButton(type: .system)
+        button.setTitle("Subscribe", for: .normal)
+        button.addAction(UIAction { [unowned self] _ in
+            self.didTapSubscribe()
+        }, for: .primaryActionTriggered)
+        self.subscribeButton = button
+    }
+    
+    private func didTapSubscribe() {
+        Task {
+            do {
+                // get paywall object with Placement ID
+                let paywall = try await Adapty.getPaywall(
+                    placementId: AppConstants.placementId
+                )
+                // configure paywall
+                let configuration = try await AdaptyUI.getPaywallConfiguration(
+                    forPaywall: paywall
+                )
+                // create controller of completed paywall screen
+                let paywallController = try? AdaptyUI.paywallController(
+                    with: configuration,
+                    delegate: self
+                )
+                // show
+                guard let paywallController = paywallController else { return }
+                present(paywallController, animated: true)
+            } catch {
+                // show alert in the case of an error
+                showAlert(title: "Error", message: error.localizedDescription)
+            }
+        }
+    }
+    
+    private func showAlert(title: String, message: String) {
+      let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+      alert.addAction(.init(title: "OK", style: .default))
+      present(alert, animated: true)
+    }
+    
+    private func viewModelReloadProfile() {
+        Task {
+            await viewModel.reloadProfile()
+        }
+    }
+}
+
+extension PayScreen: AdaptyPaywallControllerDelegate {
+    
+    // when customer finish purchasing
+    func paywallDidFinishPurchase(
+        _ controller: AdaptyPaywallController,
+        product: AdaptyPaywallProduct,
+        profile: AdaptyProfile
+    ) {
+        controller.dismiss(animated: true)
+        // TODO: reload UI
+    }
+
+    // customer pressed close button
+    func paywallDidClose(_ controller: AdaptyPaywallController) {
+        controller.dismiss(animated: true)
+    }
+    
+    // MARK: AdaptyPaywallControllerDelegate
+    
+    // Customer's purchase failed
+    func paywallController(
+        _ controller: AdaptyPaywallController,
+        didFailPurchase product: AdaptyPaywallProduct,
+        error: AdaptyError
+    ) {
+        controller.dismiss(animated: true)
+        showAlert(title: "Purchase fail", message: error.localizedDescription)
+    }
+
+    func paywallController(
+        _ controller: AdaptyPaywallController,
+        didFinishRestoreWith profile: AdaptyProfile
+    ) {
+        // handle the restore result
+    }
+
+    func paywallController(
+        _ controller: AdaptyPaywallController,
+        didFailRestoreWith error: AdaptyError
+    ) {
+        // handle the error
+    }
 }
 
 extension PayScreen {
@@ -142,104 +242,80 @@ extension PayScreen {
         view.addSubview(pageControl)
     }
     
-    func createButton() {
-        let button = UIButton(type: .system)
-        button.setTitle("Subscribe", for: .normal)
-        button.addAction(UIAction { [unowned self] _ in
-            self.didTapSubscribe()
-        }, for: .primaryActionTriggered)
-        self.subscribeButton = button
-    }
-    
-    private func didTapSubscribe() {
-        Task {
-            do {
-                // get paywall object with Placement ID
-                let paywall = try await Adapty.getPaywall(
-                    placementId: AppConstants.placementId
-                )
-                // configure paywall
-                let configuration = try await AdaptyUI.getPaywallConfiguration(
-                    forPaywall: paywall
-                )
-                // create controller of completed paywall screen
-                let paywallController = try? AdaptyUI.paywallController(
-                    with: configuration,
-                    delegate: self
-                )
-                // show
-                guard let paywallController = paywallController else { return }
-                present(paywallController, animated: true)
-            } catch {
-                // show alert in the case of an error
-                showAlert(title: "Error", message: error.localizedDescription)
-            }
+    private func setupBottomArea() {
+            view.addSubview(bottomContainer)
+            view.addSubview(subscribeButton)
+        
+            bottomContainer.backgroundColor = UIColor.purple
+            bottomContainer.layer.cornerRadius = 30
+            bottomContainer.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+            
+            priceLabel.text = "Subscribe for $0.99 weekly"
+            priceLabel.textColor = .white
+            priceLabel.font = .systemFont(ofSize: 16, weight: .medium)
+            priceLabel.textAlignment = .center
+            
+            detailLabel.text = "Plan automatically renews. Cancel anytime."
+            detailLabel.textColor = .white
+            detailLabel.font = .systemFont(ofSize: 12)
+            detailLabel.textAlignment = .center
+            
+            subscribeButton.setTitle("Subscribe", for: .normal)
+            subscribeButton.titleLabel?.font = .systemFont(ofSize: 18, weight: .bold)
+            subscribeButton.backgroundColor = UIColor.systemTeal
+            subscribeButton.layer.cornerRadius = 22
+            subscribeButton.setTitleColor(.white, for: .normal)
+            
+            termsButton.setTitle("Terms of Use", for: .normal)
+            termsButton.titleLabel?.font = .systemFont(ofSize: 12)
+            termsButton.setTitleColor(.white, for: .normal)
+            
+            privacyButton.setTitle("Privacy & Policy", for: .normal)
+            privacyButton.titleLabel?.font = .systemFont(ofSize: 12)
+            privacyButton.setTitleColor(.white, for: .normal)
+            
+            termsStack.axis = .horizontal
+            termsStack.distribution = .equalSpacing
+            termsStack.addArrangedSubview(termsButton)
+            termsStack.addArrangedSubview(privacyButton)
+            
+            bottomContainer.addSubview(priceLabel)
+            bottomContainer.addSubview(detailLabel)
+            bottomContainer.addSubview(subscribeButton)
+            bottomContainer.addSubview(termsStack)
         }
-    }
-    
-    private func showAlert(title: String, message: String) {
-      let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-      alert.addAction(.init(title: "OK", style: .default))
-      present(alert, animated: true)
-    }
-    
-    private func setupUI() {
-        view.addSubview(subscribeButton)
-    
-        subscribeButton.snp.makeConstraints { make in
+        
+    private func layoutAll() {
+        pageViewController.view.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(bottomContainer.snp.top)
+        }
+        pageControl.snp.makeConstraints { make in
+            make.top.equalTo(pageViewController.view.snp.bottom).offset(-20)
             make.centerX.equalToSuperview()
-            make.centerY.equalToSuperview()
         }
-    }
-    
-    private func viewModelReloadProfile() {
-        Task {
-            await viewModel.reloadProfile()
+        bottomContainer.snp.makeConstraints { make in
+            make.leading.trailing.bottom.equalToSuperview()
+            make.height.equalTo(view.bounds.height * 0.35)
         }
-    }
-}
-
-extension PayScreen: AdaptyPaywallControllerDelegate {
-    
-    // when customer finish purchasing
-    func paywallDidFinishPurchase(
-        _ controller: AdaptyPaywallController,
-        product: AdaptyPaywallProduct,
-        profile: AdaptyProfile
-    ) {
-        controller.dismiss(animated: true)
-        // TODO: reload UI
-    }
-
-    // customer pressed close button
-    func paywallDidClose(_ controller: AdaptyPaywallController) {
-        controller.dismiss(animated: true)
-    }
-    
-    // MARK: AdaptyPaywallControllerDelegate
-    
-    // Customer's purchase failed
-    func paywallController(
-        _ controller: AdaptyPaywallController,
-        didFailPurchase product: AdaptyPaywallProduct,
-        error: AdaptyError
-    ) {
-        controller.dismiss(animated: true)
-        showAlert(title: "Purchase fail", message: error.localizedDescription)
-    }
-
-    func paywallController(
-        _ controller: AdaptyPaywallController,
-        didFinishRestoreWith profile: AdaptyProfile
-    ) {
-        // handle the restore result
-    }
-
-    func paywallController(
-        _ controller: AdaptyPaywallController,
-        didFailRestoreWith error: AdaptyError
-    ) {
-        // handle the error
+        
+        priceLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(24)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        detailLabel.snp.makeConstraints { make in
+            make.top.equalTo(priceLabel.snp.bottom).offset(4)
+            make.leading.trailing.equalToSuperview().inset(16)
+        }
+        subscribeButton.snp.makeConstraints { make in
+            make.top.equalTo(detailLabel.snp.bottom).offset(16)
+            make.leading.trailing.equalToSuperview().inset(32)
+            make.height.equalTo(44)
+        }
+        termsStack.snp.makeConstraints { make in
+            make.top.equalTo(subscribeButton.snp.bottom).offset(12)
+            make.centerX.equalToSuperview()
+        }
     }
 }
 
